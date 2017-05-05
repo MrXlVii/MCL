3e9b2a22abca6d5c60196d6e0a790ad55c6d3066
Tasheme Thomas <tasheme.thomas@gmail.com>
2017-05-05 14:28:52 -0400

    removed commented out code

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -22,8 +22,7 @@ grad = zeros(size(theta));
 
 J = (1/m)*sum(-y'*log(sigmoid(X*theta))-(1-y)'*(log(1-sigmoid(X*theta))));
 grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
-% for i=1:length(theta)
-%   grad(i) = (1/m)*sum(sigmoid(X*theta)-y).*X;
+
 
 
 
